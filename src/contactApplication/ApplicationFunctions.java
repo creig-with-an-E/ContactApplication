9de0f2e3859932c7645f547cbf567f1a81e5caec
Creig Fortune <fortunephiri004@gmail.com>
2018-03-27 01:15:18 -0400

    addition of CRUD operations with relevant logic

--- src/contactApplication/ApplicationFunctions.java
+++ src/contactApplication/ApplicationFunctions.java
@@ -2,4 +2,20 @@ package contactApplication;
 
 public abstract class ApplicationFunctions {
 
+	public static void save() {
+		
+	}
+	
+	public static void delete(String Name) {
+		
+	}
+	
+	public static void edit() {
+		
+	}
+	
+	public static void search() {
+		
+	}
+	
 }
